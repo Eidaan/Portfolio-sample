b04755f50dfca3a0ffaf3dc3427caf82ea54e5c0
Eidaan Ansari <eidanansari1@gmail.com>
2023-08-01 21:57:48 +0530

    uploading my portfolio

---
+++ CSS website/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-Text", { 
+strings: ['Front End Developer', 'Youtuber', "Digital content creator", "Blogger"],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop: true
+})
